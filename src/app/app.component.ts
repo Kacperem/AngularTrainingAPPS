bde1e7ed68a000b6019cf35c29261dbf7d52a164
Kacperem <kacp.rembisz@gmail.com>
2019-12-02 23:09:17 +0100

    examples {{}}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTraining';
+  title = 'MyTest';
+  pi = Math.PI;
+  date = new Date();
+
+  mydog = new MyDog('Berna', 5);
+
+  showMyDog() {
+    return 'My Dog is' + this.mydog.name + 'and she is ' + this.mydog.age + ' years old';
+  }
+
+
+}
+
+class MyDog {
+  constructor(public name: string, public age: number) {
+
+  }
 }
