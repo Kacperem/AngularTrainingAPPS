22d1703e2e8c93ee04359f15808dd581a8e21527
Kacperem <kacp.rembisz@gmail.com>
2019-12-04 18:24:54 +0100

    *ngFor with index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
